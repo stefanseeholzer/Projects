0851f074eec168cd8a438fcbe1eccfab604f9b5d
stefanseeholzer <stefanseeholzer99@gmail.com>
2025-07-16 18:09:33 -0500

    Replace symlink LEGODataApp with actual directory

--- Kotlin/LEGODataApp/app/src/main/java/com/example/legodataapp/data/AllSet.kt
+++
@@ -1,8 +0,0 @@
-package com.example.legodataapp.data
-
-import com.google.gson.annotations.SerializedName
-
-data class AllSet(
-    @SerializedName("results")
-    var legoSetList: List<LegoSet>
-)
